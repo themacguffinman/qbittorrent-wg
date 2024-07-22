c893939d315b57abafdd7bdc5aecad3e9372b3c5
Joshua Thong <jyh.thong@gmail.com>
2024-07-22 12:11:12 -0400

    whoops, add UDP inbound block

--- README.md
+++ README.md
@@ -62,4 +62,6 @@ Internally, it uses `wg-quick up` to apply your Wireguard conf.
 ### Firewall 
 To ensure qBittorrent doesn't leak traffic outside the Wireguard connection, `iptables` is used to block all outbound non-Wireguard traffic, with the exception of traffic initiated from inbound connections so WebUI can work.
 
+There's also a block on all inbound Wireguard traffic to your `$WEBUI_PORT` to prevent others in the Wireguard network from accessing your qBittorrent WebUI.
+
 The `iptables` rules are defined in [s6-rc.d/init-wireguard/run_up.sh](s6-rc.d/init-wireguard/run_up.sh)
--- s6-rc.d/init-wireguard/run_up.sh
+++ s6-rc.d/init-wireguard/run_up.sh
@@ -8,7 +8,9 @@ WEBUI_PORT=${WEBUI_PORT:-8080}
 iptables -A OUTPUT ! -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT ! -o wg0 -j REJECT
 iptables -A INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
+iptables -A INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
 
 ip6tables -A OUTPUT ! -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -A OUTPUT ! -o wg0 -j REJECT
 ip6tables -A INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
+ip6tables -A INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
