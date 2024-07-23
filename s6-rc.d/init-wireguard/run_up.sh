b4789fb4b9a54bff0a830ba7cdc22f2bf93dd3f7
Joshua Thong <jyh.thong@gmail.com>
2024-07-23 01:34:08 -0400

    commented-out experimental iptables rules for blocking all inbound traffic except to the torrenting port

--- s6-rc.d/init-wireguard/run_up.sh
+++ s6-rc.d/init-wireguard/run_up.sh
@@ -5,12 +5,26 @@ wg-quick up wg0
 
 WEBUI_PORT=${WEBUI_PORT:-8080}
 
+# Block all outbound traffic outside Wireguard, except if it was initiated by an inbound connection (exception is for qBittorrent WebUI)
 iptables -A OUTPUT ! -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT ! -o wg0 -j REJECT
-iptables -A INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
-iptables -A INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
-
 ip6tables -A OUTPUT ! -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -A OUTPUT ! -o wg0 -j REJECT
+
+# Block all inbound traffic from Wireguard targeting the WebUI port, don't want Wireguard peers accessing the WebUI
+iptables -A INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
+iptables -A INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
 ip6tables -A INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
 ip6tables -A INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
+
+# TODO: Block all inbound traffic from Wireguard, except if it was initiated by an outbound connection or targets the $TORRENTING_PORT
+#iptables -A INPUT -i wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#ip6tables -A INPUT -i wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+#if [[ -n "${TORRENTING_PORT}" ]]; then
+#    iptables -A INPUT -i wg0 -p tcp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+#    iptables -A INPUT -i wg0 -p udp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+#    ip6tables -A INPUT -i wg0 -p tcp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+#    ip6tables -A INPUT -i wg0 -p udp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+#fi
+#iptables -A INPUT -i wg0 -j REJECT
+#ip6tables -A INPUT -i wg0 -j REJECT
