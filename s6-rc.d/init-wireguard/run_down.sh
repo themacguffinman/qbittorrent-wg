571ca0f9436b03dfd15eaf26d749b330a7f8d013
Joshua Thong <jyh.thong@gmail.com>
2024-07-26 03:05:40 -0400

    try "block all inbound traffic from Wireguard" rules

--- s6-rc.d/init-wireguard/run_down.sh
+++ s6-rc.d/init-wireguard/run_down.sh
@@ -9,9 +9,15 @@ iptables -D OUTPUT ! -o wg0 -j REJECT
 ip6tables -D OUTPUT ! -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -D OUTPUT ! -o wg0 -j REJECT
 
-iptables -D INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
-iptables -D INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
-ip6tables -D INPUT -i wg0 -p tcp -m multiport --dports "${WEBUI_PORT}" -j DROP
-ip6tables -D INPUT -i wg0 -p udp -m multiport --dports "${WEBUI_PORT}" -j DROP
+iptables -D INPUT -i wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+ip6tables -D INPUT -i wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+if [[ -n "${TORRENTING_PORT}" ]]; then
+    iptables -D INPUT -i wg0 -p tcp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+    iptables -D INPUT -i wg0 -p udp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+    ip6tables -D INPUT -i wg0 -p tcp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+    ip6tables -D INPUT -i wg0 -p udp -m multiport --dports "${TORRENTING_PORT}" -j ACCEPT
+fi
+iptables -D INPUT -i wg0 -j REJECT
+ip6tables -D INPUT -i wg0 -j REJECT
 
 wg-quick down wg0
